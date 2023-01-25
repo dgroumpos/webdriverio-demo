d6a304bcf16da0653f6a53d332931e00bb6085e7
Dimitris Groumpos <groumposd@unisystems.gr>
2023-01-25 13:51:26 +0200

    Completed reporting functionality with screenshot

--- utils/commands.js
+++ utils/commands.js
@@ -2,12 +2,10 @@ import allureReporter, { addStep } from "@wdio/allure-reporter";
 
 module.exports = {
   step: async function (title, body) {
-    await allureReporter.startStep(title);
+    await allureReporter.addStep(title);
+    await allureReporter.startStep();
     await body();
-    let screenshot = await browser.takeScreenshot();
-    await allureReporter.addStep("", {
-      content: await allureReporter.addAttachment("", screenshot),
-    });
+    await browser.takeScreenshot();
     await allureReporter.endStep();
   },
 };
